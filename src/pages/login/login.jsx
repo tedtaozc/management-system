728d0f67489d3b606241899b23066fbede3898ad
ted <tedtaozc@outlook.com>
2019-12-30 17:46:35 +0800

    login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -4,8 +4,34 @@ import logo from './images/logo.png';
 
 import './login.less';
 
-export default class Login extends Component {
+import { Form, Icon, Input, Button } from 'antd';
+
+class Login extends Component {
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+
+    validatePassword = (rule, value, callback) => {
+        if (!value) {
+            callback('Please input your password!');
+        } else if (value.length < 4) {
+            callback('Minimum length is 4 characters');
+        } else if (value.length > 12) {
+            callback('Maxinum length is 12 characters');
+        } else if (!/^[A-Za-z0-9_]+$/.test(value)) {
+            callback('Password should consists of letters, numbers and underlines');
+        }
+        callback();
+    }
+
     render() {
+        const { getFieldDecorator } = this.props.form;
         return (
             <div className="login">
                 <header className="login-header">
@@ -14,9 +40,49 @@ export default class Login extends Component {
                 </header>
                 <section className="login-content">
                     <h2>User Login</h2>
-                    <div>form</div>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            {getFieldDecorator('username', {
+                                rules: [
+                                    { required: true, message: 'Please input your username!' },
+                                    { min: 4, message: 'Minimum length  is 4 characters'},
+                                    { max: 12, message: 'Maximum length is 12 characters'},
+                                    { pattern: /^[A-Za-z0-9_]+$/, message: 'Username should consists of letters, numbers and underlines'}
+                                ],
+                            })(
+                                <Input
+                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    placeholder="Username"
+                                />,
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('password', {
+                            /*
+                               in {}, this.validatePassword cannot be this.valudatePassword()
+                               because {} will parse the value first.
+                            */
+                                rules: [{ validator: this.validatePassword }],
+                            })(
+                                <Input
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    type="password"
+                                    placeholder="Password"
+                                />,
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Log in
+                            </Button>
+                        </Form.Item>
+                    </Form>
                 </section>
             </div>
         );
     }
 }
+
+const WrappedLogin = Form.create()(Login);
+
+export default WrappedLogin;
